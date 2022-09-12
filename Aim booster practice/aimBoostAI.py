d047d61449c16007b85acc4083d80d9a47e2ce97
Joseph H <vanhayes618@gmail.com>
2022-09-11 23:36:44 -0500

    Instead of scanning picture then double checking do both simultaneously. Much faster, new record 3:30

--- Aim booster practice/aimBoostAI.py
+++ Aim booster practice/aimBoostAI.py
@@ -5,10 +5,13 @@ import keyboard
 import random
 import win32api, win32con
 
-time.sleep(3)
+time.sleep(2)
 xs = 580
+xf = 1325
 ys = 470
-stag = 17
+yf = 968
+
+stag = 10
 
 # screen region: [576,444],[1325,968]
 # target B value: 195
@@ -23,43 +26,17 @@ while keyboard.is_pressed('q') == False:
     pic = pyautogui.screenshot(region=(xs,ys,749,524))
     width,height = pic.size
 
-    n = 0
-    tarX = [0] * 200 #we assume the number of targetted pixels never exceeds 100
-    tarY = [0] * 200
-    
-    for x in range(0,width,stag): #first we scan the picture and save all of the target coordinates
+    for x in range(0,width,stag): #we scan the entire screenshot
         for y in range(0,height,stag):
-
-            r,g,b = pic.getpixel((x,y))
             
-            if b==195:
-                tarX[n] = x+xs
-                tarY[n] = y+ys
-                n = n+1
-
+            r,g,b = pic.getpixel((x,y)) #we save the rgb values
+            
+            if r==255: #first check the picture
+                if pyautogui.pixel((x+xs),(y+ys))[0]==255: #double check the actual screen to save time
+                    clickScreen((x+xs),(y+ys)) #authorize clicking the screen
+                    
             if keyboard.is_pressed('q') == True: #we have to check q every time or else itll take an entire screen analysis to return to while loop
                 break
         if keyboard.is_pressed('q') == True:
                 break
-
-    n = 0;
-    for x in tarX: #then we authorize a click on all targets in a row
-        for y in tarY:
-            
-            if tarX[n]==0:
-                break
-            if tarY[n]==0:
-                break
-
-            if pyautogui.pixel(x,y)[0]>220: #before clicking double check the pixel color
-                clickScreen(x,y)
-            n = n+1
-
-            if keyboard.is_pressed('q') == True: #we have to check q every time or else itll take an entire screen analysis to return to while loop
-                break
-        if keyboard.is_pressed('q') == True:
-                break
-
-
-
 
