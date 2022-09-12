3449acfe5116adc25a306d3c5ca44230560ee570
Joseph H <vanhayes618@gmail.com>
2022-09-11 22:55:44 -0500

    ai checks pixel right before clicking to avoid false positives and save time

--- Aim booster practice/aimBoostAI.py
+++ Aim booster practice/aimBoostAI.py
@@ -8,6 +8,7 @@ import win32api, win32con
 time.sleep(3)
 xs = 580
 ys = 470
+stag = 15
 
 # screen region: [576,444],[1325,968]
 # target B value: 195
@@ -15,7 +16,6 @@ ys = 470
 def clickScreen(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while keyboard.is_pressed('q') == False:
@@ -24,11 +24,11 @@ while keyboard.is_pressed('q') == False:
     width,height = pic.size
 
     n = 0
-    tarX = [0] * 100 #we assume the number of targetted pixels never exceeds 100
-    tarY = [0] * 100
+    tarX = [0] * 200 #we assume the number of targetted pixels never exceeds 100
+    tarY = [0] * 200
     
-    for x in range(0,width,5): #first we scan the picture and save all of the target coordinates
-        for y in range(0,height,5):
+    for x in range(0,width,stag): #first we scan the picture and save all of the target coordinates
+        for y in range(0,height,stag):
 
             r,g,b = pic.getpixel((x,y))
             
@@ -42,15 +42,18 @@ while keyboard.is_pressed('q') == False:
         if keyboard.is_pressed('q') == True:
                 break
 
+    n = 0;
     for x in tarX: #then we authorize a click on all targets in a row
         for y in tarY:
-
-            clickScreen(x,y)
-
+            
             if tarX[n]==0:
                 break
             if tarY[n]==0:
                 break
+
+            if pyautogui.pixel(x,y)[0]>250: #before clicking double check the pixel color
+                clickScreen(x,y)
+            n = n+1
 
             if keyboard.is_pressed('q') == True: #we have to check q every time or else itll take an entire screen analysis to return to while loop
                 break
