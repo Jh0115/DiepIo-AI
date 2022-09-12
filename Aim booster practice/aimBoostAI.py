5c5ad3220688fe311279d3bdc16b6fa930c59492
Joseph H <vanhayes618@gmail.com>
2022-09-11 21:53:00 -0500

    Fixed issue where q does not exit

--- Aim booster practice/aimBoostAI.py
+++ Aim booster practice/aimBoostAI.py
@@ -5,27 +5,37 @@ import keyboard
 import random
 import win32api, win32con
 
-time.sleep(5)
+time.sleep(1)
+xs = 576
+ys = 444
 
 # screen region: [576,444],[1325,968]
 # target B value: 195
 
-
 def clickScreen(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while keyboard.is_pressed('q') == False:
 
-    pic = pyautogui.screenshot(region=(576,444,749,524))
+    pic = pyautogui.screenshot(region=(xs,ys,749,524))
     width,height = pic.size
 
     for x in range(0,width,5):
         for y in range(0,height,5):
-            if pic.getpixel((x,y))[2]==195:
-                clickScreen((x+576),(y+444))
-                time.sleep(0.05)
+
+            r,g,b = pic.getpixel((x,y))
+            
+            if b==195:
+                clickScreen((x+xs),(y+ys))
+                time.sleep(0.1)
+
+            if keyboard.is_pressed('q') == True:
+                break
+        if keyboard.is_pressed('q') == True:
+                break
 
 
 
